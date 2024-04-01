2de8e08c753e586bd616b35ca4b16d4717d05d7b
Ashok <ashokkumarsingh@gmail.com>
2024-04-01 19:31:48 +0530

    1 April 2024: erp product code base. Basic website with nav bar. Inspiration - Jan Marshall's video

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div className="pt-4">
+      <div className="pt-[20px]">
+        <h1 className="text-center text-[32px] font-medium leading-[40px] text-[#172026]">
+          Start Managing <br/>Your Sofware Services Business
+        </h1>
+        <h1 className="text-center text-[32px] font-medium leading-[40px] text-[#4c82f7]">
+          Like a Pro!
+        </h1>
+        <p className="text-center pt-6 text-[#36485c]">
+          Capability Assessment, Know How Development, Process Auatomation,
+          Implementation and Handholding
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Logo from "../../../public/assets/Logo.svg";
+import User from "../../../public/assets/User.svg";
+import Menu from "../../../public/assets/Menu.svg";
+
+const navLinks = [
+{name: 'Services'},
+{name: 'Produts'},
+{name: 'About'},
+];
+
+export default function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:mx-auto lg:px-20">
+      <div className="flex items-center">
+        <Image src={Logo} alt="Logo" />
+        <div className="hidden lg:flex pl-[74px] gap-[56px]">
+        {navLinks.map((item, index) => (
+            <p className="text-[#36485C] font-medium" key={index}>{item.name}</p>
+        ))}
+        </div>
+      </div>
+      <div className="flex gap-x-5">
+        <p className="hidden lg:block font-medium text-[#36485C] pr-[56px]">Open an account</p>
+        <div className="flex items-center gap-x-2">
+          <Image src={User} alt="User Profile" />
+          <span className="hidden font-medium text-[#36485C] lg:block">Sign in</span> 
+        </div>
+        <div>
+          <Image src={Menu} alt="Menu" className="lg:hidden"/>
+        </div>
+      </div>
+    </nav>
+  );
+}
