e37d0d48733c24f72a69a2ca220f639a11159cbc
Ashok <ashokkumarsingh@gmail.com>
2024-04-11 11:35:30 +0530

    11 April 2024: erp product code base. Basic website with nav bar, React Icons and svg icons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 import Logo from "../../../public/assets/Logo.svg";
 import User from "../../../public/assets/User.svg";
 import Menu from "../../../public/assets/Menu.svg";
+import Sun from "../../../public/assets/Sun.svg";
+// Using local assets; good source of svg icons is https://www.svgrepo.com/
+import Moon from "../../../public/assets/Moon.svg";
+// Using React Icons from https://react-icons.github.io/react-icons/
+import { BiUser } from "react-icons/bi";
+
+
 
 const navLinks = [
 {name: 'Services'},
@@ -25,6 +32,11 @@ export default function Navbar() {
         <div className="flex items-center gap-x-2">
           <Image src={User} alt="User Profile" />
           <span className="hidden font-medium text-[#36485C] lg:block">Sign in</span> 
+        </div>
+        <div className="flex w-full item-center">
+          <Image src={Sun} alt="Sun" />
+          <Image src={Moon} alt="Moon" />
+          <BiUser />
         </div>
         <div>
           <Image src={Menu} alt="Menu" className="lg:hidden"/>
